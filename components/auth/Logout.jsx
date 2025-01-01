f2b2ef8ca30091a3ed1cbeef26ea59a0a64564e6
sifatbro786 <solaimanislamsifat@gmail.com>
2025-01-01 14:47:01 +0600

    Show Authenticated User details in Profile

--- components/auth/Logout.jsx
+++ components/auth/Logout.jsx
@@ -5,6 +5,7 @@ import { signOut } from "next-auth/react";
 export default function Logout() {
     return (
         <button
+            className="px-[6px] border-[3px] border-primary"
             onClick={() => signOut({ callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/login` })}
         >
             Sign Out
