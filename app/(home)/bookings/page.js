976ed76a8294acd5c0de2e88129c70ebcf484b08
sifatbro786 <solaimanislamsifat@gmail.com>
2025-01-03 01:03:31 +0600

    Implement My Profile Booking List

--- app/(home)/bookings/page.js
+++ app/(home)/bookings/page.js
@@ -4,12 +4,25 @@ import UpcomingBooking from "@/components/user/booking/UpcomingBooking";
 import { auth } from "@/auth";
 
 import { redirect } from "next/navigation";
+import { getBookingsByUser, getUserByEmail } from "@/database/queries";
 
 const BookingsPage = async () => {
     const session = await auth();
+
     if (!session) {
         redirect("/login");
     }
+
+    const loggedInUser = await getUserByEmail(session?.user?.email);
+    const bookings = await getBookingsByUser(loggedInUser?.id);
+
+    const pastBookings = bookings.filter((booking) => {
+        return new Date().getTime() > new Date(booking?.checkin).getTime();
+    });
+
+    const upcomingBookings = bookings.filter((booking) => {
+        return new Date().getTime() < new Date(booking?.checkin).getTime();
+    });
 
     return (
         <>
@@ -21,8 +34,8 @@ const BookingsPage = async () => {
             <section>
                 <div className="container">
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                        <PastBooking />
-                        <UpcomingBooking />
+                        <PastBooking bookings={pastBookings} />
+                        <UpcomingBooking bookings={upcomingBookings} />
                     </div>
                 </div>
             </section>
